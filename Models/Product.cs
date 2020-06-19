2aa8dfde817acc0478ac3eafc73e475c215cc7d8
Vrunda402 <66813452+Vrunda402@users.noreply.github.com>
2020-06-18 23:36:51 -0400

    Add files via upload

---
+++ Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace GetJsonData.Models
+{
+    public class Product
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+
+        [JsonPropertyName("img")]
+        public string title { get; set; }
+        
+        
+        public int[] Ratings { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
+    }
+}
